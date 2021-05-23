5e6a83754baf5c695a9928882a141d49d1ee4220
汐风 <1162246687@qq.com>
2021-05-23 15:25:22 +0800

    关机

---
+++ 关机/关机/1.c
@@ -0,0 +1,36 @@
+# define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+//int main()
+//{
+//	char arr[20] = { 0 };
+//	system("shutdown -s -t 30");
+//again:
+//	printf("您的电脑将在30秒后关机，如果输入我是猪即可取消关机，\n请输入：");
+//	scanf("%s", &arr);
+//	if (strcmp(arr, "我是猪") == 0)
+//		system("shutdown -a");
+//	else
+//		goto again;
+//	return 0;
+//}
+//int main()
+//{
+//	int i = 0;
+//	for (i = 1; i < 10; i++)
+//	{
+//		int a = 0;
+//		for (a = 0; a < i; a++)
+//		{
+//			printf("%2d*%2d=%2d", a + 1, i, (a + 1)*i);
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+#include<stdio.h>
+	int main() {
+		printf("\"Hello word!\"");
+			return 0;
+	}
